6e32f2f386271895e4a27e7055c808444c7650e2
Piotrek <@>
2017-12-30 15:16:00 +0100

    przeslanie plikow

---
+++ Rozw10_3/Osoba.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozw10_3
+{
+    class Osoba
+    {
+        string imie;
+        int wiek;
+        Plec plec;
+        DateTime dataUrodzenia;
+
+        public Osoba(string imie, Plec plec, DateTime dataUrodzenia)
+        {
+            this.imie = imie;
+            this.plec = plec;
+            this.dataUrodzenia = dataUrodzenia;
+            this.wiek = DateTime.Now.Year - dataUrodzenia.Year;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Imię: {0}, płeć: {1}, wiek: {2}, data urodzenia: {3}", imie, plec, wiek, dataUrodzenia.ToString("dd.MM.yyyy"));
+        }
+
+    }
+}
---
+++ Rozw10_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozw10_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Osoba> osoby = new Queue<Osoba>();
+            osoby.Enqueue(new Osoba("Jan", Plec.M, new DateTime(2000, 11, 22)));
+            osoby.Enqueue(new Osoba("Anna", Plec.K, new DateTime(1980, 4, 30)));
+            osoby.Enqueue(new Osoba("Zenon", Plec.M, new DateTime(1990, 7, 4)));
+            foreach (var element in osoby)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadKey();
+        }
+    }
+}
